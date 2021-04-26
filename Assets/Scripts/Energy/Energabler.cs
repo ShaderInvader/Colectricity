3d8101a8aa75fe4da321f940061405006a6fc23a
Mateusz1010 <lolmanek123456@gmail.com>
2021-04-26 14:37:37 +0200

    Tasks with ball sizes and containers color done

--- Assets/Scripts/Energy/Energabler.cs
+++ Assets/Scripts/Energy/Energabler.cs
@@ -3,6 +3,28 @@ public class Energabler : MonoBehaviour
 {
     public int energy = 0;
     public int max_energy = 100;
+    MeshRenderer renderer;
+
+    private void Start()
+    {
+        renderer = GetComponent<MeshRenderer>();
+        renderer.materials[0].SetFloat("_EmissiveIntensity", 0.2f);
+    }
+
+    private void Update()
+    {
+        if(gameObject.tag == "Container")
+        {
+            Color c = new Color(93, 203, 255);
+            renderer.materials[0].SetColor("_EmissiveColor", Color.Lerp(Color.black, c, 0.3f * ((float)energy) / max_energy));
+        }
+        else // players
+        {
+            float s = 0.5f * ((float)energy) / max_energy + 1;
+            transform.localScale = new Vector3(s, s, s);
+        }
+    }
+
     public bool AddEnergy()
     {
         if (IsFull())
@@ -31,5 +53,19 @@ public class Energabler : MonoBehaviour
     public bool IsEmpty()
     {
         return energy == 0;
+    }
+
+    public static void UpdateEmissiveColorFromIntensityAndEmissiveColorLDR(Material material)
+    {
+        const string kEmissiveColorLDR = "_EmissiveColorLDR";
+        const string kEmissiveColor = "_EmissiveColor";
+        const string kEmissiveIntensity = "_EmissiveIntensity";
+
+        if (material.HasProperty(kEmissiveColorLDR) && material.HasProperty(kEmissiveIntensity) && material.HasProperty(kEmissiveColor))
+        {
+            Color emissiveColorLDR = material.GetColor(kEmissiveColorLDR);
+            Color emissiveColorLDRLinear = new Color(Mathf.GammaToLinearSpace(emissiveColorLDR.r), Mathf.GammaToLinearSpace(emissiveColorLDR.g), Mathf.GammaToLinearSpace(emissiveColorLDR.b));
+            material.SetColor(kEmissiveColor, emissiveColorLDRLinear * material.GetFloat(kEmissiveIntensity));
+        }
     }
 }
