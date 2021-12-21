f20eca1c0ff801ae9bb8bea79269fda2720e7e64
Mateusz1010 <lolmanek123456@gmail.com>
2021-12-21 19:20:07 +0100

    Light intensity change in Battery and Door prefabs

--- Assets/Scripts/Energy/EnergablerLight.cs
+++ Assets/Scripts/Energy/EnergablerLight.cs
@@ -5,9 +5,21 @@ using UnityEngine;
 public class EnergablerLight : MonoBehaviour
 {
     private Light light;
+    private Energabler energabler;
+
+    public int minLightIntensity = 0;
+    public int maxLightIntensity = 20000;
 
     void Start()
     {
         light = GetComponentInChildren<Light>();
+        energabler = GetComponent<Energabler>();
+        if (energabler == null) Debug.LogError("Energabler Light needs Energabler in the same Object!");
+        if (light == null) Debug.LogError("Energabler Light needs Light in the children Object!");
+    }
+
+    private void Update()
+    {
+        light.intensity = Mathf.Lerp(minLightIntensity, maxLightIntensity, ((float)energabler.energy_units) / ((float)energabler.max_energy_units));
     }
 }
