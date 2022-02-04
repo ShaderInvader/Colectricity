b1165db11870bec1d9d2af2dd144b14d84702ed0
Bart63 <bartek.durys@gmail.com>
2022-02-04 22:15:56 +0100

    Loading screen

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,11 +1,31 @@
+using System.Collections;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
+    public GameObject loadingScreen;
+    public Slider slider;
+    public Text percentage;
+
     public void StartGame()
     {
-        SceneManager.LoadScene("1");
+        StartCoroutine(LoadAsynchronously("1"));
+    }
+
+    IEnumerator LoadAsynchronously(string sceneIndex)
+    {
+        loadingScreen.SetActive(true);
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            slider.value = progress;
+            percentage.text = progress * 100 + "%";
+            yield return null;
+        }
     }
 
     public void QuitGame()
