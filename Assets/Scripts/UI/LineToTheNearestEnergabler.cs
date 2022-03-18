077f3f14b32d9d4c2e12c5fd4111f4d9ac8ff163
Mateusz1010 <lolmanek123456@gmail.com>
2022-03-18 21:02:24 +0100

    linia dodana

--- Assets/Scripts/UI/LineToTheNearestEnergabler.cs
+++ Assets/Scripts/UI/LineToTheNearestEnergabler.cs
@@ -10,7 +10,9 @@ public class LineToTheNearestEnergabler : MonoBehaviour
     public float minDistanceWithMaxOpacity;
     public float minOpacity;
     public float maxOpacity;
-    public Transform line;
+    public Material material;
+
+   private List<GameObject> lines = new List<GameObject>();
 
     void Start()
     {
@@ -19,26 +21,47 @@ public class LineToTheNearestEnergabler : MonoBehaviour
 
     void Update()
     {
-        Energabler[] energablers = FindObjectsOfType<Energabler>();
-        foreach (Energabler energabler in energablers)
+        foreach(GameObject line in lines)
         {
-            float distance = Vector3.Distance(transform.position, energabler.transform.position);
-            if (energabler.GetComponent<Electron>() != null)
+            Destroy(line);
+        }
+        Energabler[] energablers = GameObject.FindObjectsOfType<Energabler>();
+        foreach(Energabler e in energablers) /* usun to */
+        {
+            float distance = Vector3.Distance(transform.position, e.transform.position);
+            if (e.GetComponent<Electron>() != null)
             {
-                break;
+                continue;
             }
             else if (distance < minDistanceWithMinOpacity)
             {
                 float opacity;
                 if (distance < minDistanceWithMaxOpacity) opacity = maxOpacity;
-                else opacity = Mathf.Lerp(maxOpacity, minOpacity, (distance - minDistanceWithMaxOpacity) / (minDistanceWithMinOpacity - minDistanceWithMaxOpacity));
+                else opacity = Mathf.Lerp(maxOpacity, minOpacity, (distance - minDistanceWithMaxOpacity) / (minDistanceWithMinOpacity - minDistanceWithMaxOpacity)) / maxOpacity;
 
-                if ((type == Electron.Type.giver && energabler.energy_units < energabler.max_energy_units)
-                    || (type == Electron.Type.receiver && energabler.energy_units > 0))
+                if ((type == Electron.Type.giver && e.energy_units < e.max_energy_units && GetComponent<Energabler>().energy_units > 0)
+                    || (type == Electron.Type.receiver && e.energy_units > 0))
                 {
-                    Debug.DrawLine(transform.position, energabler.transform.position, Color.green);
+                    DrawLine(this.transform.position, e.transform.position, material);
                 }
             }
         }
+    }
+
+    void DrawLine(Vector3 start, Vector3 end, Material material)
+    {
+        GameObject myLine = new GameObject();
+        myLine.transform.position = start;
+        myLine.AddComponent<LineRenderer>();
+        LineRenderer lr = myLine.GetComponent<LineRenderer>();
+        if(material != null)
+        {
+            lr.material = material;
+        }
+        lr.startWidth = 0.1f;
+        lr.endWidth = 0.1f;
+        lr.SetPosition(0, start);
+        lr.SetPosition(1, end);
+        lines.Add(myLine);
     }
 }
