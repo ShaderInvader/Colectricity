41b1c58ede7d1050f98d2c4202214491b4979552
Mateusz1010 <lolmanek123456@gmail.com>
2022-03-29 18:07:14 +0200

    Deleting debugging stuff

--- Assets/Scripts/UI/ChangeControllerText.cs
+++ Assets/Scripts/UI/ChangeControllerText.cs
@@ -11,10 +11,8 @@ public class ChangeControllerText : MonoBehaviour
             Transform[] texts = this.GetComponentsInChildren<Transform>(true);
             foreach (Transform t in texts)
             {
-                Debug.Log(t.gameObject.name);
                 if(t.gameObject.name == ControllerInfo.getObjectName(controllerType)) 
                 {
-                    Debug.Log("Pasuje " + t.gameObject.name + " " + ControllerInfo.getObjectName(controllerType));
                     if (controllerType == ControllerInfo.controllerPick)
                     {
                         t.gameObject.SetActive(true);
