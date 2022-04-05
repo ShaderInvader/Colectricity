3fb3bb1a0f8456a8328c15bef0c93bb3eec21bfb
Bart63 <bartek.durys@gmail.com>
2022-04-05 22:44:08 +0200

    Rotate heads

---
+++ Assets/Scripts/Movement/SmoothRotation.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothRotation : MonoBehaviour
+{
+    public MovementPrototype mp;
+    public float speed = 0.1f;
+
+    void Update()
+    {
+        Vector3 targetDirection = new Vector3(-mp.movement_vector.z, 0, mp.movement_vector.x);
+        Debug.Log(mp.movement_vector);
+        float singleStep = speed * Time.deltaTime;
+        Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
+        transform.rotation = Quaternion.LookRotation(newDirection);
+    }
+}
