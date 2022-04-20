759d2c21cf65c567d80d82581f5eeefb462e288e
Mateusz1010 <lolmanek123456@gmail.com>
2022-04-20 22:31:08 +0200

    Indicators rotation done

--- Assets/Scripts/Camera/Indicator.cs
+++ Assets/Scripts/Camera/Indicator.cs
@@ -27,21 +27,53 @@ public class Indicator : MonoBehaviour
                     Vector3 end = thisCamera.WorldToViewportPoint(indicator.transform.position) -
                     thisCamera.WorldToViewportPoint(this.transform.position);
 
+                    float angle = 0; 
+                       
                     if (Mathf.Abs(end.x) > Mathf.Abs(end.y))
                     {
                         end = new Vector3(Mathf.Sign(end.x) * indicatorDistanceFromScreen, Mathf.Sign(end.x) * (indicatorDistanceFromScreen * end.y) / end.x, 0);
+                        angle = (Mathf.Rad2Deg * Mathf.Asin(end.y / end.magnitude) * Mathf.Sign(getAdjustedDifference(end))) + getAdjustedDifference(end);
                     }
                     else
                     {
                         end = new Vector3(Mathf.Sign(end.y) * (indicatorDistanceFromScreen * end.x) / end.y, Mathf.Sign(end.y) * indicatorDistanceFromScreen, 0);
+                        angle = (Mathf.Rad2Deg * Mathf.Asin(end.x / end.magnitude) * Mathf.Sign(getAdjustedDifference(end))) + getAdjustedDifference(end);
                     }
+                    
                     end = thisCamera.WorldToViewportPoint(this.transform.position) + end;
                     end = thisCamera.ViewportToScreenPoint(end);
                     end.z = 0;
                     image.transform.position = end;
+                    image.localEulerAngles = new Vector3(0, 0, angle);
                 }
             }
-        }   
+        }
+    }
+
+    private float getAdjustedDifference(Vector3 end)
+    {
+        if (Mathf.Abs(end.x) > Mathf.Abs(end.y))
+        {
+            if (end.x > 0) // arrow is on right
+            {
+                return 180;
+            }
+            else // arrow is on left
+            {
+                return -0.000000000001f;
+            }
+        }
+        else // arrow is on left
+        {
+            if (end.y > 0) // arrow is top
+            {
+                return -90;
+            }
+            else // arrow is down
+            {
+                return 90;
+            }
+        }
     }
 
     private float constraint(float a, float min, float max)
