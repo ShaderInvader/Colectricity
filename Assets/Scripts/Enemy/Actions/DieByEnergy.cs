019a791c7bdfafecdf463b1a7487b412ec27af68
Filip Mazurek <f.maz.98@outlook.com>
2022-05-26 00:44:41 +0200

    Enemy fixed

--- Assets/Scripts/Enemy/Actions/DieByEnergy.cs
+++ Assets/Scripts/Enemy/Actions/DieByEnergy.cs
@@ -5,18 +5,12 @@ using UnityEngine;
 public class DieByEnergy : MonoBehaviour
 {
     public int energyUnitsToDie = 3;
-    public Color endColor;
-    Color startColor;
     int current = 0, previous=0;
     int max;
-    new MeshRenderer renderer;
 
     private void Start()
     {
         max = energyUnitsToDie;
-        renderer = GetComponent<MeshRenderer>();
-        startColor = renderer.materials[0].GetColor("_BaseColor");
-        ChangeColor();
     }
 
     void Update()
@@ -25,17 +19,11 @@ public class DieByEnergy : MonoBehaviour
         if (previous>current)
         {
             energyUnitsToDie -= (previous - current);
-            ChangeColor();
             if (energyUnitsToDie==0)
             {
                 Destroy(gameObject);
             }
         }
         previous = current;
-    }
-
-    void ChangeColor()
-    {
-        renderer.materials[0].SetColor("_BaseColor", Color.Lerp(endColor, startColor, (float)energyUnitsToDie/max));
     }
 }
